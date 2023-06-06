e3080ab147549c6d1de5cff416c6cf938f90c845
hayden365 <ysmh100@gmail.com>
2023-06-07 05:14:17 +0900

    post update

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -1,5 +1,6 @@
 import { SimplePost } from "@/model/post";
-import { client, urlFor } from "./sanity";
+import { assetsURL, client, urlFor } from "./sanity";
+import { log } from "console";
 
 const simplePostProjection = `
 ...,
@@ -106,5 +107,34 @@ export async function addComment(
 		])
 		.commit({
 			autoGenerateArrayKeys: true,
+		});
+}
+
+export async function createPost(userId: string, text: string, file: Blob) {
+	return fetch(assetsURL, {
+		method: "POST",
+		headers: {
+			"content-type": file.type,
+			authorization: `Bearer ${process.env.SANITY_SECRET_TOKEN}`,
+		},
+		body: file,
+	})
+		.then(res => res.json())
+		.then(result => {
+			return client.create(
+				{
+					_type: "post",
+					author: { _ref: userId },
+					photo: { asset: { _ref: result.document._id } },
+					comments: [
+						{
+							comment: text,
+							author: { _ref: userId, _type: "reference" },
+						},
+					],
+					likes: [],
+				},
+				{ autoGenerateArrayKeys: true },
+			);
 		});
 }
